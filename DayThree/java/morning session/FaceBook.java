519ad4665a47462731d62888208e28a4f24e611d
Malere1 <adamudivine9@email.com>
2026-05-20 12:28:21 +0100

    add new files

---
+++ DayThree/java/morning session/FaceBook.java
@@ -0,0 +1,19 @@
+public class FaceBook {
+public static void FaceBook() 
+ if(nameofpeople.equal[]) {
+ return "no one like this";
+ } else {if(nameofpeople.equal["peter"]) 
+ return "peter like this";
+ } else if(nameofpeople.equal["jacob","Alex"]) {
+ return  "jacob and  Alex like this";
+} else if(nameofpeople.equal["max","john","mark"]) {
+ return "max john and mark like this";
+ } else if(nameofpeople.equal["Alex","jacob","mark","max"]) {
+ return "Alex jacob  and two others like this";
+ }
+public static void main(String[]args ) {
+System.out.println("name");
+}
+
+}
+
--- DayTwo/python/level 5/def add_negative_numbers.py
+++ DayTwo/python/level 5/def add_negative_numbers.py
@@ -1,4 +1,4 @@
 def add_negative_numbers(array):
-    arr[:] = [x if x < 0 else 0 for x in arr]
+    array[:] = [x if x < 0 else 0 for x in array]
     return array
 
